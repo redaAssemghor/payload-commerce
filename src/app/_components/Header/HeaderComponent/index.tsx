31e315e696e69ecaed1f5731de8f2fd0c875470b
redaAssemghor <assemghor.reda@gmail.com>
2024-04-27 20:01:11 +0100

    bugg fix

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -23,7 +23,7 @@ const HeaderComponent = ({ header }: { header: Header }) => {
     >
       <Gutter className={classes.wrap}>
         <Link href="/">
-          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+          <p className={classes.logo}>Tech Store.</p>
         </Link>
 
         <HeaderNav header={header} />
